5e53e5f14eafec6386964328a0750417d238674f
Johan Guldmyr <johan.guldmyr@csc.fi>
2018-02-22 09:44:33 +0200

    Add disabled-community
    It's now possible to send a community only when the route is down or
    disabled.

--- lib/exabgp/application/healthcheck.py
+++ lib/exabgp/application/healthcheck.py
@@ -166,6 +166,9 @@ def parse ():
     g.add_argument("--community", metavar="COMMUNITY",
                    type=str, default=None,
                    help="announce IPs with the supplied community")
+    g.add_argument("--disabled-community", metavar="DISABLEDCOMMUNITY",
+                   type=str, default=None,
+                   help="announce IPs with the supplied community when disabled")
     g.add_argument("--as-path", metavar="ASPATH",
                    type=str, default=None,
                    help="announce IPs with the supplied as-path")
@@ -386,9 +389,14 @@ def loop (options):
                                                            options.next_hop or "self")
             if command == "announce":
                 announce = "{0} med {1}".format(announce, metric)
-                if options.community:
-                    announce = "{0} community [ {1} ]".format(announce,
-                                                              options.community)
+
+		if options.community or options.disabled_community:
+                    community = options.community
+                    if target in (states.DOWN, states.DISABLED) and options.disabled_community:
+                        community = options.disabled_community
+                    if community:
+                        announce = "{0} community [ {1} ]".format(announce, community)
+
                 if options.as_path:
                     announce = "{0} as-path [ {1} ]".format(announce,
                                                               options.as_path)
